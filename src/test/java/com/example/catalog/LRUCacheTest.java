9a76bccc8203b294c64d54333a06fc5db0f51519
AdamRayann <129179113+AdamRayann@users.noreply.github.com>
2024-12-12 10:08:37 +0200

    Cache tests added

--- src/test/java/com/example/catalog/LRUCacheTest.java
+++ src/test/java/com/example/catalog/LRUCacheTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class LRUCacheTest {
 
     LRUCache<String, String> cache;
@@ -22,13 +25,15 @@ class LRUCacheTest {
         @Test
         @DisplayName("cache is initially empty")
         void isEmpty() {
-            // TODO assert cache is empty
+            assertEquals(0,cache.size());
         }
 
         @Test
         @DisplayName("throws NullPointerException when getting a null key")
         void throwsExceptionWhenGettingNullKey() {
             // TODO assert NullPointerException thrown on `cache.get(null)`
+            assertThrows(NullPointerException.class,()->cache.get(null));
+
         }
 
         @Nested
@@ -37,13 +42,19 @@ class LRUCacheTest {
 
             @BeforeEach
             void addElements() {
-                // TODO add 2 elements
+                cache.set("k1","v1");
+                cache.set("k2","v2");
+
             }
 
             @Test
             @DisplayName("cache contains the added elements")
             void containsAddedElements() {
                 // TODO assert the added 2 elements are available
+                assertEquals("v1",cache.get("k1"));
+                assertEquals("v2",cache.get("k2"));
+
+
             }
         }
 
@@ -53,7 +64,9 @@ class LRUCacheTest {
 
             @BeforeEach
             void addElements() {
-                // TODO add 3 elements
+                cache.set("k1","v1");
+                cache.set("k2","v2");
+                cache.set("k3","v3");
             }
 
             @Nested
@@ -67,6 +80,7 @@ class LRUCacheTest {
                 @BeforeEach
                 void clearCache() {
                     // TODO clear the cache after
+                    cache.clear();
                 }
             }
         }
