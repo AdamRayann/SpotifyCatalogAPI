b3e3f21e1510882e2cf6bed77bca385a3a4ffab3
AdamRayann <129179113+AdamRayann@users.noreply.github.com>
2025-01-30 10:33:26 +0200

    Saving current changes

---
+++ src/main/java/com/example/catalog/services/DatabaseDataSourceService.java
@@ -0,0 +1,13 @@
+package com.example.catalog.services;
+
+import com.example.catalog.model.Artist;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseDataSourceService implements DataSourceService {
+
+    @Override
+    public Artist getArtistById(String id) {
+        return db.findById(id).orElse(null);
+    }
+}
